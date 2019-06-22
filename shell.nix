155f812eb220dce2406ff585815db4a1674b72af
Eric Bailey <e.bailey@sportradar.com>
2019-06-23 00:31:00 +0200

    Set up noweb/LaTeX environment

--- shell.nix
+++ shell.nix
@@ -1,9 +1,35 @@
-{ pkgs ? import <nixpkgs> {}
-, mkShell ? pkgs.mkShell
-}:
+with import <nixpkgs> {
+  overlays = [
+    (import <nur> {}).repos.yurrriq.overlays.nur
+    (self: super: {
+      xelatex-noweb = (super.texlive.combine {
+        inherit (super) noweb;
+        inherit (super.texlive) scheme-small
+          datetime
+          dirtytalk
+          fmtcount
+          framed
+          fvextra
+          hardwrap
+          ifplatform
+          latexmk
+          minted
+          titlesec
+          todonotes
+          tufte-latex
+          xetex
+          xstring;
+      });
+    })
+  ];
+};
 
 mkShell {
   buildInputs = with pkgs; [
     go_1_12
+    iosevka
+    noweb
+    # which
+    xelatex-noweb
   ];
 }
